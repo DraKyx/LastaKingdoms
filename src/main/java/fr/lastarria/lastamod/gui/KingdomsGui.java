6eb7bd31cdee104dfb017665d0d141f1323e9c67
DraKyxx <les.xkillers@gmail.com>
2022-08-10 16:00:31 +0200

    Gui

--- src/main/java/fr/lastarria/lastamod/gui/KingdomsGui.java
+++ src/main/java/fr/lastarria/lastamod/gui/KingdomsGui.java
@@ -18,16 +18,28 @@ import java.util.Random;
 
 public class KingdomsGui extends Screen {
 
-    private static final int TITLE_HEIGHT = 8;
-
-    public KingdomsGui() {
+    public KingdomsGui()
+    {
         super(new TranslationTextComponent("lastamod.kingdoms_gui.title", "LastaMod"));
+    }
+
+    @Override
+    protected void init()
+    {
+        this.addButton(new Button((this.width - 200) / 2, this.height - 50, 200, 20,
+                new TranslationTextComponent("lastamod.kingdoms_gui.done_button"), button -> this.onClose()));
     }
 
     @Override
     public void render(MatrixStack matrixStack, int mouseX, int mouseY, float partialTicks)
     {
-        drawCenteredString(matrixStack, this.font, this.title.getString(), this.width / 2, TITLE_HEIGHT, 0xFFFFFF);
+        drawCenteredString(matrixStack, this.font, this.title.getString(), this.width / 2, 8, 0xFFFFFF);
         super.render(matrixStack, mouseX, mouseY, partialTicks);
+    }
+
+    @Override
+    public boolean isPauseScreen()
+    {
+        return false;
     }
 }
