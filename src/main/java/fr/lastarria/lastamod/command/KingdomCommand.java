2346e16ec95caa865f6b9c71b151c18e470d8e00
DraKyxx <les.xkillers@gmail.com>
2022-08-10 14:46:17 +0200

    nsm

--- src/main/java/fr/lastarria/lastamod/command/KingdomCommand.java
+++ src/main/java/fr/lastarria/lastamod/command/KingdomCommand.java
@@ -4,10 +4,11 @@ import com.mojang.brigadier.CommandDispatcher;
 import com.mojang.brigadier.builder.LiteralArgumentBuilder;
 import fr.lastarria.lastamod.gui.KingdomsGui;
 import net.minecraft.client.Minecraft;
-import net.minecraft.client.gui.screen.inventory.InventoryScreen;
 import net.minecraft.command.CommandSource;
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.inventory.Inventory;
 import net.minecraft.util.text.ITextComponent;
+import net.minecraftforge.fml.network.NetworkHooks;
 
 public class KingdomCommand {
 
@@ -17,7 +18,7 @@ public class KingdomCommand {
                 LiteralArgumentBuilder.<CommandSource>literal("kingdom")
                         .requires(src -> src.hasPermission(0))
                         .executes(ctx -> {
-                            Minecraft.getInstance().setScreen(new InventoryScreen((PlayerEntity) ctx.getSource().getEntity()));
+                            Minecraft.getInstance().setScreen(new KingdomsGui());
                             return 0;
                         })
         );
--- src/main/java/fr/lastarria/lastamod/gui/KingdomsGui.java
+++ src/main/java/fr/lastarria/lastamod/gui/KingdomsGui.java
@@ -1,6 +1,7 @@
 package fr.lastarria.lastamod.gui;
 
 import com.mojang.blaze3d.matrix.MatrixStack;
+import fr.lastarria.lastamod.Main;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.screen.Screen;
 import net.minecraft.client.gui.screen.inventory.ContainerScreen;
@@ -8,6 +9,7 @@ import net.minecraft.client.gui.widget.button.Button;
 import net.minecraft.inventory.container.Container;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.Items;
+import net.minecraft.util.ResourceLocation;
 import net.minecraft.util.text.ITextComponent;
 import net.minecraft.util.text.StringTextComponent;
 import net.minecraft.util.text.TranslationTextComponent;
@@ -16,16 +18,16 @@ import java.util.Random;
 
 public class KingdomsGui extends Screen {
 
+    private static final int TITLE_HEIGHT = 8;
 
-    protected KingdomsGui(ITextComponent p_i51108_1_) {
-        super(p_i51108_1_);
+    public KingdomsGui() {
+        super(new TranslationTextComponent("lastamod.kingdoms_gui.title", "LastaMod"));
     }
 
     @Override
-    public void render(MatrixStack p_230430_1_, int p_230430_2_, int p_230430_3_, float p_230430_4_) {
-
-        super.render(p_230430_1_, p_230430_2_, p_230430_3_, p_230430_4_);
-        Minecraft.getInstance().getItemRenderer().renderGuiItem(new ItemStack(Items.DIAMOND_AXE.getItem()), this.width / 2 - 8, this.height / 2 - 20);
-
+    public void render(MatrixStack matrixStack, int mouseX, int mouseY, float partialTicks)
+    {
+        drawCenteredString(matrixStack, this.font, this.title.getString(), this.width / 2, TITLE_HEIGHT, 0xFFFFFF);
+        super.render(matrixStack, mouseX, mouseY, partialTicks);
     }
 }
